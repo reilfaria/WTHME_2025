f6c908c164acc101858412f5375b121e0e62b263
reilfaria <3332230046@untirta.ac.id>
2026-04-27 04:05:13 +0700

    nama

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WTHME {{ config('app.name') }}</title>
+    <title>WTHME 2025</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="icon" href="{{ asset('images/logo-kegiatan.png') }}" type="image/png">
 </head>
